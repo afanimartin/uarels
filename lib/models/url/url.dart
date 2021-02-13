114819f1a30485a83c6afbc1d79661d2b35a6d74
Martin Afani <martin@orderly.ai>
2021-02-13 12:59:49 +0300

    Add a field 'isPrivate' to Url model to filter private from public urls

--- lib/models/url/url.dart
+++ lib/models/url/url.dart
@@ -1,37 +1,42 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
-import 'package:uarels/utils/url_metadata.dart';
+import '../../utils/url_metadata.dart';
 
 class Url extends Equatable {
   final String urlId, inputUrl, title, description, imageUrl;
+  final bool isPrivate;
 
   const Url(
       {@required this.urlId,
       @required this.inputUrl,
       this.title,
       this.description,
-      this.imageUrl});
+      this.imageUrl,
+      this.isPrivate = false});
 
   Url copyWith(
           {String urlId,
           String inputUrl,
           String title,
           String description,
-          String imageUrl}) =>
+          String imageUrl,
+          bool isPrivate}) =>
       Url(
           urlId: urlId ?? this.urlId,
           inputUrl: inputUrl ?? this.inputUrl,
           title: title ?? this.title,
           description: description ?? this.description,
-          imageUrl: imageUrl ?? this.imageUrl);
+          imageUrl: imageUrl ?? this.imageUrl,
+          isPrivate: isPrivate ?? this.isPrivate);
 
   factory Url.fromSnapshot(DocumentSnapshot doc) => Url(
       urlId: doc.id,
       inputUrl: doc.data()['inputUrl'] as String,
       title: doc.data()['title'] as String,
       description: doc.data()['description'] as String,
-      imageUrl: doc.data()['imageUrl'] as String);
+      imageUrl: doc.data()['imageUrl'] as String,
+      isPrivate: doc.data()['isPrivate'] as bool);
 
   Future<Map<String, dynamic>> toDocument() async {
     final metadata = await UrlMetadata.metadata(inputUrl);
@@ -41,10 +46,12 @@ class Url extends Equatable {
       'inputUrl': inputUrl,
       'title': metadata.title,
       'description': metadata.description,
-      'imageUrl': metadata.image
+      'imageUrl': metadata.image,
+      'isPrivate': isPrivate
     };
   }
 
   @override
-  List<Object> get props => [urlId, inputUrl, title, description, imageUrl];
+  List<Object> get props =>
+      [urlId, inputUrl, title, description, imageUrl, isPrivate];
 }
